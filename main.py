84711d6f8e1999fd632ccea6ae1b967e94f12169
lukwqe <kermannadehzda@gmail.com>
2025-11-14 14:16:10 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import pygame
+from os import path
 
 pygame.init()
-screen = pygame.display.set_mode((800, 600))  # окно 800x600
-pygame.display.set_caption("My Game")          # заголовок окна
+pygame.mixer.init()
+screen = pygame.display.set_mode((800, 400))  # окно 800x600
+pygame.display.set_caption("Pusheen Game")          # заголовок окна
 clock = pygame.time.Clock()
 
-player_img = pygame.image.load("assets/player.png").convert_alpha()
-player_img = pygame.transform.scale(player_img, (80, 80))
-player_rect = player_img.get_rect(center=(400, 300))
+music_path = path.join("snd", "mp3.ogg")  # путь к музыке
+pygame.mixer.music.load(music_path)
+pygame.mixer.music.set_volume(0.4)
+pygame.mixer.music.play(-1)
+
+background_img = pygame.image.load("assets/bg.png").convert()
+background_img = pygame.transform.scale(background_img, (800, 400))
+
+player_img = pygame.image.load("assets/player.png").convert_alpha() 
+player_img = pygame.transform.scale(player_img, (110, 110))
+player_rect = player_img.get_rect(center=(400, 500))
 
 gravity = 0.8
 jump_power = -15
@@ -29,7 +39,6 @@ while running:
         player_rect.x -= speed
     if keys[pygame.K_RIGHT]:
         player_rect.x += speed
-        
     if keys[pygame.K_UP] and on_ground:
         player_y_speed = jump_power
         on_ground = False
@@ -37,8 +46,8 @@ while running:
     player_y_speed += gravity
     player_rect.y += player_y_speed     
     
-    if player_rect.bottom >= 550:
-        player_rect.bottom = 550
+    if player_rect.bottom >= 380:
+        player_rect.bottom = 380
         player_y_speed = 0
         on_ground = True     
 
@@ -48,7 +57,7 @@ while running:
     if player_rect.right > 800:
         player_rect.right = 800
 
-    screen.fill((30, 30, 30))  # фон
+    screen.blit(background_img, (0, 0))
     screen.blit(player_img, player_rect)  # рисуем игрока
     pygame.display.flip()  # обновляем экран
     clock.tick(60)
